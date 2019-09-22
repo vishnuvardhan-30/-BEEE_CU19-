44a4f56846f9928b7543fa3855c4b18a42388e29
vishnu vardhan <55591236+vishnuvardhan-30@users.noreply.github.com>
2019-09-22 18:50:35 +0530

    Add files via upload

---
+++ Exp1_LED_Flasher.ino
@@ -0,0 +1,33 @@
+void setup()
+{
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+void loop()
+{
+  digitalWrite(8,HIGH);
+  delay(100);
+  digitalWrite(8,LOW);
+  delay(100);
+  digitalWrite(9,HIGH);
+  delay(100);
+  digitalWrite(9,LOW);
+  delay(100);
+  digitalWrite(9,HIGH);
+  delay(100);
+  digitalWrite(9,LOW);
+  delay(100);
+  digitalWrite(10,HIGH);
+  delay(100);
+  digitalWrite(10,LOW);
+  delay(100);
+  digitalWrite(10,HIGH);
+  delay(100);
+  digitalWrite(10,LOW);
+  delay(100);
+  digitalWrite(10,HIGH);
+  delay(100);
+  digitalWrite(10,LOW);
+  delay(100);
+}
